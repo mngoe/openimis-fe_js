587c89e1a0efeb4518efbf8185cddf1f36c04efa
michellewis <kouamkouammichellewis@gmail.com>
2025-05-30 06:23:49 -0400

    feat(app): changement de logo

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import ModulesManagerProvider from "./ModulesManagerProvider";
 import { App, FatalError, baseApiUrl, apiHeaders } from "@openimis/fe-core";
 import messages_ref from "./translations/ref.json";
 import "./index.css";
-import logo from "./openIMIS.png";
+import logo from "./LogoAMG_v3-1.png";
 
 const loadConfiguration = async () => {
   const response = await fetch(`${baseApiUrl}/graphql`, {
