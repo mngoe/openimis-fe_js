add16ddae214b096aa688f015cd8fe51c13d8356
paul sandjong <princesandjong777@gmail.com>
2025-11-03 13:14:04 +0100

    set sentry capture for test-csu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
+import * as Sentry from "@sentry/react";
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider, LinearProgress } from "@material-ui/core";
@@ -15,7 +16,16 @@ import ModulesManagerProvider from "./ModulesManagerProvider";
 import { App, FatalError, baseApiUrl, apiHeaders } from "@openimis/fe-core";
 import messages_ref from "./translations/ref.json";
 import "./index.css";
-import logo from "./LOGOMINSANTEok.jpg"; 
+import logo from "./LOGOMINSANTEok.jpg";
+
+Sentry.init({ 
+  dsn: "https://f2741df242f344c6b46aa251ad870f2f@glitchtip-csuapps.minsante.cm/2", 
+  debug: false,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 const loadConfiguration = async () => {
   const response = await fetch(`${baseApiUrl}/graphql`, {
@@ -24,6 +34,7 @@ const loadConfiguration = async () => {
     body: JSON.stringify({ "query": "{ moduleConfigurations { module, config, controls{ field, usage } } }" }),
   });
   if (!response.ok) {
+    Sentry.captureException(new Error(`${response.status} ${response.statusText}`));
     throw response;
   } else {
     const { data } = await response.json();
@@ -46,17 +57,20 @@ const AppContainer = () => {
 
   useEffect(() => {
     loadConfiguration().then(
-      (config) =>
+      (config) => {
         setAppState({
           error: null,
           isLoading: false,
           config,
-        }),
-      (error) =>
+        });
+      },
+      (error) => {
+        Sentry.captureException(new Error("Failed to load configuration"));
         setAppState({
           error,
           isLoading: false,
-        }),
+        });
+      }
     );
   }, []);
 
@@ -103,5 +117,14 @@ const AppContainer = () => {
   }
 };
 
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
+ReactDOM.render(
+  <Sentry.ErrorBoundary
+    fallback={<FatalError error={{ code: 500, message: "An unexpected error occurred" }} />}
+    showDialog
+  >
+    <AppContainer />
+  </Sentry.ErrorBoundary>,
+  document.getElementById("root")
+);
+
 serviceWorker.register();
