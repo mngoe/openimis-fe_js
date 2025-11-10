c5725420e2095ee3a73f03cd4a9982e6de8245f2
paul sandjong <princesandjong777@gmail.com>
2025-11-10 12:03:17 +0100

    corrections

--- src/index.js
+++ src/index.js
@@ -18,15 +18,6 @@ import messages_ref from "./translations/ref.json";
 import "./index.css";
 import logo from "./LOGOMINSANTEok.jpg";
 
-Sentry.init({ 
-  dsn: process.env.REACT_APP_SENTRY_DSN,
-  debug: false,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-});
-
 const loadConfiguration = async () => {
   const response = await fetch(`${baseApiUrl}/graphql`, {
     method: "post",
@@ -65,7 +56,6 @@ const AppContainer = () => {
         });
       },
       (error) => {
-        Sentry.captureException(new Error("Failed to load configuration"));
         setAppState({
           error,
           isLoading: false,
@@ -91,6 +81,16 @@ const AppContainer = () => {
     );
   } else {
     const modulesManager = new ModulesManager(appState.config);
+    const sentryDSN = modulesManager.getConf("fe", "sentryDSN", "");
+    if (sentryDSN) {
+      Sentry.init({ 
+        dsn: sentryDSN,
+        debug: false,
+        integrations: [
+          Sentry.browserTracingIntegration(),
+        ],
+      });
+    }
     const reducers = modulesManager.getContribs("reducers").reduce((reds, red) => {
       reds[red.key] = red.reducer;
       return reds;
